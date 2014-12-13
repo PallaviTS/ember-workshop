93ea57a08bc8873e0f48b19f93e1375b908f2044
PallaviTS <pallavi046@gmail.com>
2014-12-13 14:24:09 +0530

    Intial commit

--- Brocfile.js
+++ Brocfile.js
@@ -17,4 +17,25 @@ var app = new EmberApp();
 // please specify an object with the list of modules as keys
 // along with the exports of each module as its value.
 
-module.exports = app.toTree();
+var pickFiles = require('broccoli-static-compiler');
+var mergeTrees = require('broccoli-merge-trees');
+
+app.import('bower_components/bootstrap/dist/css/bootstrap.min.css');
+app.import('bower_components/bootstrap-material-design/dist/css/material.min.css');
+app.import('bower_components/bootstrap-material-design/dist/css/ripples.min.css');
+app.import('bower_components/bootstrap-material-design/dist/css/material-wfont.min.css');
+
+app.import('bower_components/bootstrap-material-design/dist/js/material.js');
+app.import('bower_components/bootstrap-material-design/dist/js/ripples.js');
+
+
+var materialDesignFontFiles = pickFiles('bower_components/bootstrap-material-design/dist/fonts', {
+  srcDir: '/',
+  files: ['**/*.woff', '**/*.eot', '**/*.svg', '**/*.ttf'],
+  destDir: '/fonts'
+});
+
+
+// Providing additional trees to the `toTree` method will result in those
+// trees being merged in the final output.
+module.exports = mergeTrees([app.toTree(), materialDesignFontFiles]);
